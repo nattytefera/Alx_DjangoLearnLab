f7c0a1d5e8657b6d0f04fd711f292a9fa873abd5
nattytefera <nhattysafari@gmail.com>
2025-03-09 23:55:13 +0300

    configuring authentication

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -127,6 +127,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication", 
-    ]
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
